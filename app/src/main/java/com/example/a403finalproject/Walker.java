e5d6e6251716e732e86fe4239e67e66211ded71c
mediocre251 <70655924+mediocre251@users.noreply.github.com>
2023-11-28 09:13:58 -0500

    manifest stuff

--- app/src/main/java/com/example/a403finalproject/Walker.java
+++ app/src/main/java/com/example/a403finalproject/Walker.java
@@ -17,9 +17,9 @@ public class Walker {
     String Email;
     double Lattitude;
     double Longitude;
+    int TUID;
 
-
-    public Walker(String fName, String lName, String address, String state, String country, boolean isWalker, double walkRate, String sDesc, double charge, String lDesc, String phoneNumber, String email, double lattitude, double longitude) {
+    public Walker(String fName, String lName, String address, String state, String country, boolean isWalker, double walkRate, String sDesc, double charge, String lDesc, String phoneNumber, String email, double lattitude, double longitude, int TUID) {
         this.fName = fName;
         this.lName = lName;
         this.address = address;
@@ -33,6 +33,120 @@ public class Walker {
         PhoneNumber = phoneNumber;
         Email = email;
         Lattitude = lattitude;
+        Longitude = longitude;
+        this.TUID = TUID;
+    }
+
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isWalker() {
+        return isWalker;
+    }
+
+    public void setWalker(boolean walker) {
+        isWalker = walker;
+    }
+
+    public double getWalkRate() {
+        return walkRate;
+    }
+
+    public void setWalkRate(double walkRate) {
+        this.walkRate = walkRate;
+    }
+
+    public String getsDesc() {
+        return sDesc;
+    }
+
+    public void setsDesc(String sDesc) {
+        this.sDesc = sDesc;
+    }
+
+    public double getCharge() {
+        return Charge;
+    }
+
+    public void setCharge(double charge) {
+        Charge = charge;
+    }
+
+    public String getlDesc() {
+        return lDesc;
+    }
+
+    public void setlDesc(String lDesc) {
+        this.lDesc = lDesc;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public double getLattitude() {
+        return Lattitude;
+    }
+
+    public void setLattitude(double lattitude) {
+        Lattitude = lattitude;
+    }
+
+    public double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(double longitude) {
         Longitude = longitude;
     }
 }
--- app/src/main/java/com/example/a403finalproject/WalkerList.java
+++ app/src/main/java/com/example/a403finalproject/WalkerList.java
@@ -3,9 +3,19 @@ package com.example.a403finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WalkerList extends AppCompatActivity {
 
@@ -31,31 +41,58 @@ public class WalkerList extends AppCompatActivity {
 
     }
 
-
-/*
-    public void getData(){
+    public void getData() {
         String url = "https://cs403api20231121223109.azurewebsites.net/SVSU_CS403/GetAllWalkers";
-        JsonObjectRequest r = new JsonObjectRequest(Request.Method.GET, url, null,
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
                 response -> {
-
                     try {
-                        JSONArray categoriesArray = response.getJSONArray("trivia_categories");
-                        for(int i = 0; i < categoriesArray.length(); i++){
-                            JSONObject categoryObj = categoriesArray.getJSONObject(i);
-                            int id = categoryObj.getInt("id");
-                            String name = categoryObj.getString("name");
-                            Category c = new Category(name, id);
-                            category.add(c);
-                        }
-                        adapter.notifyDataSetChanged();
+                        List<Walker> walkers = new ArrayList<>();
 
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject walkerObj = response.getJSONObject(i);
+
+                            // Parse walker details
+                            int tuId = walkerObj.getInt("TuID");
+                            String firstName = walkerObj.getString("first_name");
+                            String lastName = walkerObj.getString("last_name");
+                            String phoneNumber = walkerObj.getString("phone_number");
+                            String email = walkerObj.getString("email");
+                            // ... (similarly parse other fields)
+
+                            double latitude = walkerObj.getDouble("latitude");
+                            double longitude = walkerObj.getDouble("longitude");
+
+                            // Create a Person object
+                            Walker walker = new Person();
+                            walker.setTuID(tuId);
+                            walker.setFirstName(firstName);
+                            walker.setLastName(lastName);
+                            walker.setPhoneNumber(phoneNumber);
+                            walker.setEmail(email);
+                            // ... (similarly set other fields)
+
+                            walker.setLatitude(latitude);
+                            walker.setLongitude(longitude);
+
+                            walkers.add(walker);
+                        }
+
+                        // Process the list of walkers as needed
+                        // Note: You may want to update the UI or perform other actions with the walker data.
+
+                    } catch (JSONException e) {
+                        Log.e("Error", "Error parsing JSON response", e);
                     } catch (JSONException e) {
                         throw new RuntimeException(e);
                     }
                 },
-                error -> Log.d("DDD",error.toString()));
-        queue.add(r);
+                error -> Log.e("Error", "Error fetching data: " + error.toString()));
+
+        // Add the request to the request queue
+        queue.add(request);
     }
-    */
+
+
+
 
 }
