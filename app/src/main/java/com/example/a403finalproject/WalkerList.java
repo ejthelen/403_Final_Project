83c2664e999a915e71a270ed4ec5d7a660e324cf
mediocre251 <70655924+mediocre251@users.noreply.github.com>
2023-11-30 08:08:19 -0500

    added necessary listeners for filter button along with editext.

--- app/src/main/java/com/example/a403finalproject/WalkerList.java
+++ app/src/main/java/com/example/a403finalproject/WalkerList.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -38,6 +41,7 @@ public class WalkerList extends AppCompatActivity {
     String streetAddress;
     String City;
     String State;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,8 +76,26 @@ public class WalkerList extends AppCompatActivity {
         lstWalkers.setAdapter(adapter);
 
 
+        btnFilter.setOnClickListener(e->{
 
+        });
 
+        edFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void getData() {
