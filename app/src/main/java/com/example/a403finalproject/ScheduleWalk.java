2ef22ad6ab3770f57ffb9bc3f48a21e78bf4abd3
gwaldenb <110639045+gwaldenb@users.noreply.github.com>
2023-12-04 18:28:09 -0500

    added am/pm controlls for the time scheduler section.

--- app/src/main/java/com/example/a403finalproject/ScheduleWalk.java
+++ app/src/main/java/com/example/a403finalproject/ScheduleWalk.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.time.LocalDateTime;
 import java.util.Calendar;
@@ -16,6 +19,8 @@ public class ScheduleWalk extends AppCompatActivity {
     CalendarView cvDate;
     EditText edTime;
 
+    RadioButton rdoAM, rdoPM;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +29,28 @@ public class ScheduleWalk extends AppCompatActivity {
         btnConfirmBooking = findViewById(R.id.btnConfirmBooking);
         cvDate = findViewById(R.id.cvDate);
         edTime = findViewById(R.id.edTime);
+        rdoAM = findViewById(R.id.rdoAM);
+        rdoPM = findViewById(R.id.rdoPM);
+
+        rdoPM.setChecked(true);
 
         Calendar c = Calendar.getInstance();
 
         cvDate.setDate(c.getTimeInMillis());
-        edTime.setText("12:00");
+        edTime.setText("12:01");
 
 
         btnConfirmBooking.setOnClickListener(e->{
 
         });
+
+        rdoAM.setOnCheckedChangeListener((buttonView, isChecked) -> rdoPM.setChecked(false));
+
+        rdoPM.setOnCheckedChangeListener((buttonView, isChecked) -> rdoAM.setChecked(false));
+
+
+
     }
+
+
 }
